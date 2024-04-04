23283223167de769c59ddb800d1eb0f9d3b1f8a8
newby <newby@rakis.net>
2024-04-04 09:40:18 -0700

    Cheaty sudo wrap

--- restart.sh
+++ restart.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+function work() {
+  docker pull qwtflive/updater
+  source .env.production
+  docker-compose -f production.yml down
+  docker-compose -f production.yml up -d
+}
+WORK=$(declare -f work)
+
 git pull --ff-only
-source .env.production
-docker-compose -f production.yml down
-docker-compose -f production.yml up -d
+
+sudo bash -c "$WORK; work;"
 
